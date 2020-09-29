80cd8c3b3ed34a7e8412649caea9d4092c6889ed
Emmanouela Pothitou <emmanouelapothitou@gmail.com>
2020-09-29 22:08:11 +0300

    Updates

---
+++ precache-manifest.a517344a859fe0f6b9355f9d0e4f8903.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f3958109b4f321ae3442e95c997c74",
+    "url": "/weather-app-react-sample/index.html"
+  },
+  {
+    "revision": "8da53168c11685cabfba",
+    "url": "/weather-app-react-sample/static/css/main.623f1e0a.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app-react-sample/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react-sample/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da53168c11685cabfba",
+    "url": "/weather-app-react-sample/static/js/main.3c562899.chunk.js"
+  },
+  {
+    "revision": "9c6de1431b89aca76253",
+    "url": "/weather-app-react-sample/static/js/runtime-main.a8cb7a4f.js"
+  },
+  {
+    "revision": "f19160f1e49868e68b0d6323529f1da7",
+    "url": "/weather-app-react-sample/static/media/autumn.f19160f1.jpg"
+  },
+  {
+    "revision": "6389391fcbd4564cb70c7e0572f99e65",
+    "url": "/weather-app-react-sample/static/media/spring.6389391f.jpg"
+  },
+  {
+    "revision": "42004064b3d0e9c2704bbf83aeb1c925",
+    "url": "/weather-app-react-sample/static/media/summer.42004064.jpg"
+  },
+  {
+    "revision": "7deca9b00316654af1dd58e2e970f0f2",
+    "url": "/weather-app-react-sample/static/media/winter.7deca9b0.jpg"
+  }
+]);
